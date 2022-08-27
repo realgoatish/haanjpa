78016f17631ae91c8560d2eb146a277d8b540f06
Michael Lamb <mflambjr@gmail.com>
2022-08-26 21:56:49 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { H, Section } from 'tusculum';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<Section>
+	<H level="+1">This is a test</H>
+</Section>
