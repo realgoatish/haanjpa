660ff43f2f68a195633babc71c560596e11ddf5b
Michael Lamb <mflambjr@gmail.com>
2022-08-26 22:33:27 -0400

    cloudflare env var support

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,15 @@
 declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
-	// interface Platform {}
+	interface Platform {
+    env: {
+      COUNTER: DurableObjectNamespace;
+    }
+    context: {
+      waitUntil(promise: Promise<any>): void;
+    }
+    caches: CacheStorage & { default: Cache }
+  }
 	// interface PrivateEnv {}
 	// interface PublicEnv {}
 }
