d9d482af9c9187b2d4d4c0d2d55fc6c012b280e4
Mike Lamb <mflambjr@gmail.com>
2022-12-22 02:03:58 -0500

    TODO comment

--- src/lib/js/actions.js
+++ src/lib/js/actions.js
@@ -1,3 +1,4 @@
+// TODO this is a progressive enhancement for collapsible navs & is not being used on our site
 export function enhanceHeaderNav(node) {
 	const button = document.createElement('button');
 	button.setAttribute('aria-expanded', 'false');
