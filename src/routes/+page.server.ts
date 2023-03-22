74c3a37f3d3c41d84469b01bb44726617be848c5
Mike Lamb <mflambjr@gmail.com>
2023-03-21 21:44:43 -0400

    process page ogImage before sending data to frontend

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,19 @@
 import { error } from '@sveltejs/kit';
 import { homePageQuery } from '$lib/js/sanityQueries.server'
 import { client } from '$lib/js/sanityClient.server'
+import { processOgImageUrls } from '$lib/js/sanityImages.server'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 
 	// const response = await client.fetch(homePageQuery)
-  const response = await client.fetch(homePageQuery)
+  const response = await client.fetch(homePageQuery).then(data => {
+    const processedOgImages = processOgImageUrls(data.ogImage)
+
+    data.ogImage = processedOgImages
+
+    return data
+  })
 
 	if (response) {
 		return response;
